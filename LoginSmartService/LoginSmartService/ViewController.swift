79655b9e545809c8854074e67804fbe311b13da6
Eduardo119623 <49211957+Eduardo119623@users.noreply.github.com>
2019-04-02 15:38:40 -0600

    Add files via upload

---
+++ LoginSmartService/LoginSmartService/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginSmartService
+//
+//  Created by Esfera Estudio S de RL de CV on 4/2/19.
+//  Copyright © 2019 Esfera Estudio S de RL de CV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
